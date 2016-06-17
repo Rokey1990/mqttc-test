c7f6fffcea796bf935e59e665f2a398fcf27e0c6
Rokey1990 <992030970@qq.com>
2016-06-17 11:39:33 +0800

    Initial Commit

---
+++ MqttMutiClient/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MqttMutiClient
+//
+//  Created by anjubao on 16/6/17.
+//  Copyright © 2016年 anjubao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
